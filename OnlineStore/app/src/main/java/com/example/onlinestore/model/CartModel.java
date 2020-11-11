9a38e1215ef0d7047b5408e9ea18216826c65b5c
Sangeev Neupane <61389338+SangeevNeupane5757@users.noreply.github.com>
2020-11-11 13:22:39 -0600

    files added

---
+++ OnlineStore/app/src/main/java/com/example/onlinestore/model/CartModel.java
@@ -0,0 +1,74 @@
+package com.example.onlinestore.model;
+
+public class CartModel {
+
+    String orderDate,orderNo,urlImage,productName,productId;
+    int qty,id;
+    double price;
+    public CartModel(){}
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
+
